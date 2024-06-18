1b75d61c7ca653485dfee14d7ceb308ad805b66c
Forgemaster Gua <53552547+Clevergua@users.noreply.github.com>
2024-06-18 15:35:00 +0800

    Create CMakeLists & script

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+    struct timespec timespec;
+    timespec_get(&timespec, TIME_UTC);
+    char buff[100];
+
+    strftime(buff, sizeof buff, "%D %T", gmtime(&timespec.tv_sec));
+    printf("Current time: %s.%09ld UTC\n", buff, timespec.tv_nsec);
+    printf("Raw timespec.tv_sec: %jd\n", timespec.tv_sec);
+    printf("Raw timespec.tv_nsec: %09ld\n", timespec.tv_nsec);
+    printf("Hello Tickernel!");
+}
