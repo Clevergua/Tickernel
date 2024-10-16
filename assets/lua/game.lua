21926a69dd0ec6f4f8e935353b666e65df73e7ea
Forgemaster Gua <53552547+Clevergua@users.noreply.github.com>
2024-10-16 18:22:01 +0800

    Update

--- assets/lua/game.lua
+++ assets/lua/game.lua
@@ -3,25 +3,21 @@ local gameMath = require("gameMath")
 local game = {}
 
 function game.GenerateWorld(length, width)
-    local world = {
-        length = length,
-        width = width,
-        temperatureMap = {},
-        humidityMap = {},
-        terrainMap = {},
-        manaMap = {},
-    }
+    local length = length
+    local width = width
+    local temperatureMap = {}
+    local humidityMap = {}
+    local terrainMap = {}
+    local manaMap = {}
     for x = 1, length do
-        world.temperatureMap[x] = {}
-        world.humidityMap[x] = {}
-        world.terrainMap[x] = {}
-        world.manaMap[x] = {}
+        temperatureMap[x] = {}
+        humidityMap[x] = {}
+        terrainMap[x] = {}
+        manaMap[x] = {}
         for y = 1, width do
             gameMath.PerlinNoise2D(0, x, y)
         end
     end
-
-    return world
 end
 
 function game.Start()
