6893062c07606e12bb05bed5372d1dc797d4d170
he.ma <Clevergua@outlook.com>
2024-12-13 18:07:10 +0800

    Update

--- src/postProcessRenderPass.c
+++ src/postProcessRenderPass.c
@@ -6,11 +6,11 @@ static void CreateVkRenderPass(PostProcessRenderPass *pPostProcessRenderPass, Vk
         .format = colorVkFormat,
         .samples = VK_SAMPLE_COUNT_1_BIT,
         .loadOp = VK_ATTACHMENT_LOAD_OP_LOAD,
-        .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+        .storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
         .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
         .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
-        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
-        .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+        .initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+        .finalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
     };
     VkAttachmentDescription swapchainAttachmentDescription = {
         .flags = 0,
@@ -29,70 +29,32 @@ static void CreateVkRenderPass(PostProcessRenderPass *pPostProcessRenderPass, Vk
         swapchainAttachmentDescription,
     };
 
-    VkAttachmentReference opaqueLightingColorAttachmentReference = {
+    VkAttachmentReference colorAttachmentReference = {
         .attachment = 0,
         .layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
     };
-    VkAttachmentReference opaqueLightingColorAttachmentReference = {
+    VkAttachmentReference swapchainAttachmentReference = {
         .attachment = 1,
         .layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
     };
 
-    VkSubpassDescription geometrySubpassDescription = {
+    VkSubpassDescription postProcessSubpassDescription = {
         .flags = 0,
         .pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
-        .inputAttachmentCount = 0,
-        .pInputAttachments = NULL,
+        .inputAttachmentCount = 1,
+        .pInputAttachments = &colorAttachmentReference,
         .colorAttachmentCount = 1,
-        .pColorAttachments = colorAttachments,
-        .pResolveAttachments = NULL,
-        .pDepthStencilAttachment = &opaqueGeometryDepthAttachmentReference,
-        .preserveAttachmentCount = 0,
-        .pPreserveAttachments = NULL,
-    };
-
-    VkAttachmentReference opaqueLightingDepthAttachmentReference = {
-        .attachment = 1,
-        .layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
-    };
-    VkAttachmentReference opaqueLightingAlbedoAttachmentReference = {
-        .attachment = 2,
-        .layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-    };
-    VkAttachmentReference opaqueLightingNormalAttachmentReference = {
-        .attachment = 3,
-        .layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-    };
-    VkSubpassDescription ligthtingSubpassDescription = {
-        .flags = 0,
-        .pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
-        .inputAttachmentCount = 3,
-        .pInputAttachments = (const VkAttachmentReference[]){opaqueLightingDepthAttachmentReference, opaqueLightingAlbedoAttachmentReference, opaqueLightingNormalAttachmentReference},
-        .colorAttachmentCount = 1,
-        .pColorAttachments = &opaqueLightingColorAttachmentReference,
+        .pColorAttachments = &swapchainAttachmentReference,
         .pResolveAttachments = NULL,
         .pDepthStencilAttachment = NULL,
         .preserveAttachmentCount = 0,
         .pPreserveAttachments = NULL,
     };
 
-    uint32_t subpassCount = 2;
+    uint32_t subpassCount = 1;
     VkSubpassDescription vkSubpassDescriptions[] = {
-        geometrySubpassDescription,
-        ligthtingSubpassDescription,
+        postProcessSubpassDescription,
     };
-    uint32_t dependencyCount = subpassCount - 1;
-    VkSubpassDependency subpassDependencies[dependencyCount];
-    for (uint32_t i = 0; i < dependencyCount; i++)
-    {
-        subpassDependencies[i].srcSubpass = i;
-        subpassDependencies[i].dstSubpass = i + 1;
-        subpassDependencies[i].srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-        subpassDependencies[i].dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-        subpassDependencies[i].srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
-        subpassDependencies[i].dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT;
-        subpassDependencies[i].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
-    }
 
     VkRenderPassCreateInfo vkRenderPassCreateInfo = {
         .sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
@@ -102,43 +64,74 @@ static void CreateVkRenderPass(PostProcessRenderPass *pPostProcessRenderPass, Vk
         .pAttachments = vkAttachmentDescriptions,
         .subpassCount = subpassCount,
         .pSubpasses = vkSubpassDescriptions,
-        .dependencyCount = dependencyCount,
-        .pDependencies = subpassDependencies,
+        .dependencyCount = 0,
+        .pDependencies = NULL,
     };
     VkResult result = VK_SUCCESS;
-    result = vkCreateRenderPass(vkDevice, &vkRenderPassCreateInfo, NULL, &pDeferredRenderPass->vkRenderPass);
+    result = vkCreateRenderPass(vkDevice, &vkRenderPassCreateInfo, NULL, &pPostProcessRenderPass->vkRenderPass);
     TryThrowVulkanError(result);
 }
-static void DestroyVkRenderPass(DeferredRenderPass *pDeferredRenderPass, VkDevice vkDevice)
+static void DestroyVkRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice)
 {
-    vkDestroyRenderPass(vkDevice, pDeferredRenderPass->vkRenderPass, NULL);
+    vkDestroyRenderPass(vkDevice, pPostProcessRenderPass->vkRenderPass, NULL);
 }
 
-void CreatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, VkViewport viewport, VkRect2D scissor)
+void CreatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, VkViewport viewport, VkRect2D scissor, uint32_t swapchainCount, VkImageView *swapchainImageViews)
 {
-    CreateVkRenderPass(pDeferredRenderPass, vkDevice, colorGraphicImage.vkFormat, depthGraphicImage.vkFormat, albedoGraphicImage.vkFormat, normalGraphicImage.vkFormat);
-    VkImageView attachments[] = {colorGraphicImage.vkImageView, depthGraphicImage.vkImageView, albedoGraphicImage.vkImageView, normalGraphicImage.vkImageView};
-    VkFramebufferCreateInfo vkFramebufferCreateInfo = {
-        .sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-        .pNext = NULL,
-        .flags = 0,
-        .renderPass = pDeferredRenderPass->vkRenderPass,
-        .attachmentCount = 4,
-        .pAttachments = attachments,
-        .width = viewport.width,
-        .height = viewport.height,
-        .layers = 1,
-    };
-    VkResult result = vkCreateFramebuffer(vkDevice, &vkFramebufferCreateInfo, NULL, &pDeferredRenderPass->vkFramebuffer);
-    TryThrowVulkanError(result);
 
+    CreateVkRenderPass(pPostProcessRenderPass, vkDevice, colorGraphicImage.vkFormat, colorGraphicImage.vkFormat);
+    pPostProcessRenderPass->vkFramebufferCount = swapchainCount;
+    for (size_t i = 0; i < swapchainCount; i++)
+    {
+        VkImageView attachments[] = {swapchainImageViews[i]};
+        VkFramebufferCreateInfo vkFramebufferCreateInfo = {
+            .sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+            .pNext = NULL,
+            .flags = 0,
+            .renderPass = pPostProcessRenderPass->vkRenderPass,
+            .attachmentCount = 1,
+            .pAttachments = attachments,
+            .width = viewport.width,
+            .height = viewport.height,
+            .layers = 1,
+        };
+        VkResult result = vkCreateFramebuffer(vkDevice, &vkFramebufferCreateInfo, NULL, &pPostProcessRenderPass->vkFramebuffers[i]);
+        TryThrowVulkanError(result);
+    }
     uint32_t subpassIndex = 0;
-    CreateOpaqueGeometrySubpass(&pDeferredRenderPass->opaqueGeometrySubpass, pDeferredRenderPass->shadersPath, pDeferredRenderPass->vkRenderPass, subpassIndex, vkDevice, viewport, scissor);
-    subpassIndex++;
-    CreateOpaqueLightingSubpass(&pDeferredRenderPass->opaqueLightingSubpass, pDeferredRenderPass->shadersPath, pDeferredRenderPass->vkRenderPass, subpassIndex, vkDevice, viewport, scissor, globalUniformBuffer, lightsUniformBuffer, depthGraphicImage.vkImageView, albedoGraphicImage.vkImageView, normalGraphicImage.vkImageView);
+    CreatePostProcessSubpass(&pPostProcessRenderPass->postProcessSubpass, pPostProcessRenderPass->shadersPath, pPostProcessRenderPass->vkRenderPass, subpassIndex, vkDevice, viewport, scissor);
 }
 void DestroyPostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice)
 {
+    for (size_t i = 0; i < pPostProcessRenderPass->vkFramebufferCount; i++)
+    {
+        vkDestroyFramebuffer(vkDevice, pPostProcessRenderPass->vkFramebuffers[i], NULL);
+    }
+    DestroyVkRenderPass(pPostProcessRenderPass, vkDevice);
 }
-void UpdatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, uint32_t width, uint32_t height);
-void RecordPostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkCommandBuffer vkCommandBuffer, VkViewport viewport, VkRect2D scissor, VkDevice vkDevice);
+void UpdatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, uint32_t width, uint32_t height, VkImageView *swapchainImageViews)
+{
+    for (size_t i = 0; i < pPostProcessRenderPass->vkFramebufferCount; i++)
+    {
+        vkDestroyFramebuffer(vkDevice, pPostProcessRenderPass->vkFramebuffers[i], NULL);
+
+        VkImageView attachments[] = {swapchainImageViews[i]};
+        VkFramebufferCreateInfo vkFramebufferCreateInfo = {
+            .sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+            .pNext = NULL,
+            .flags = 0,
+            .renderPass = pPostProcessRenderPass->vkRenderPass,
+            .attachmentCount = 1,
+            .pAttachments = attachments,
+            .width = width,
+            .height = height,
+            .layers = 1,
+        };
+        VkResult result = vkCreateFramebuffer(vkDevice, &vkFramebufferCreateInfo, NULL, &pPostProcessRenderPass->vkFramebuffers[i]);
+        TryThrowVulkanError(result);
+    }
+}
+void RecordPostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkCommandBuffer vkCommandBuffer, VkViewport viewport, VkRect2D scissor, VkDevice vkDevice)
+{
+    
+}
--- src/postProcessRenderPass.h
+++ src/postProcessRenderPass.h
@@ -5,11 +5,12 @@ typedef struct PostProcessRenderPassStruct
 {
     char *shadersPath;
     VkRenderPass vkRenderPass;
-    VkFramebuffer vkFramebuffer;
+    uint32_t vkFramebufferCount;
+    VkFramebuffer *vkFramebuffers;
     Subpass postProcessSubpass;
 } PostProcessRenderPass;
 
-void CreatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, VkViewport viewport, VkRect2D scissor);
+void CreatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, VkViewport viewport, VkRect2D scissor, uint32_t swapchainCount, VkImageView *swapchainImageViews);
 void DestroyPostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice);
-void UpdatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage,  uint32_t width, uint32_t height);
+void UpdatePostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkDevice vkDevice, GraphicImage colorGraphicImage, uint32_t width, uint32_t height, VkImageView *swapchainImageViews);
 void RecordPostProcessRenderPass(PostProcessRenderPass *pPostProcessRenderPass, VkCommandBuffer vkCommandBuffer, VkViewport viewport, VkRect2D scissor, VkDevice vkDevice);
